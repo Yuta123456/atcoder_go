8c7975fbc518b6cd27ecad2c5851766fa0f325be
Yuta123456 <yuuta09090530@icloud.com>
2022-11-08 07:38:12 +0900

    others

--- abc190/d/main.go
+++ abc190/d/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("Yes")
+	var n int
+	fmt.Scan(&n)
+	ans := 0
+	// div_2n := make([]int, 0)
+	for div := 1; div <= int(math.Sqrt(2.0*float64(n))); div++ {
+		if (2*n)%div == 0 {
+			a := div
+			b := 2 * n / div
+			if a%2 != b%2 {
+				ans += 2
+				if a == b {
+					ans -= 1
+				}
+			}
+		}
+	}
+	fmt.Println(ans)
 }
--- abc193/c/main.go
+++ abc193/c/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("Yes")
+	var n int
+	fmt.Scan(&n)
+	flag := make(map[int]bool)
+	for i := 2; i < int(10e6); i++ {
+		if flag[i] {
+			// fmt.Println(i, " skip")
+			continue
+		}
+		for p := 2; int(math.Pow(float64(i), float64(p))) <= n; p++ {
+			// fmt.Println(i, p)
+			flag[int(math.Pow(float64(i), float64(p)))] = true
+		}
+	}
+	// fmt.Println(flag)
+	fmt.Println(n - len(flag))
 }
